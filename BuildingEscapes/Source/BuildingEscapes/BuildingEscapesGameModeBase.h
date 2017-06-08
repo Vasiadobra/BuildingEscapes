2f6a536b17df00a6d47df0f304b9adacd36bd42b
Vasiadobra <vasiadobra@gmail.com>
2017-06-08 10:27:08 +0300

    try one

---
+++ BuildingEscapes/Source/BuildingEscapes/BuildingEscapesGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPES_API ABuildingEscapesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
